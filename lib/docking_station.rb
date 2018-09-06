c2a5fe4c53ea564f7e6964721ad404732a321d44
shweta patil <shwetapatil@shwetas-MacBook-Pro.local>
2018-09-06 15:28:27 +0100

    17. Initialization default

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,9 +1,34 @@
-require "bike"
+require_relative "./bike"
 class DockingStation
-  def release_bike
-    Bike.new
+  attr_reader :bikes, :capacity
+
+  def initialize(capacity = 20)
+    @bikes = []
+    @capacity = capacity
   end
+
+  def release_bike
+    fail 'No bikes available' if empty?
+    @bikes.pop
+  end
+
+  def dock(bike)
+    fail "Dock station is full" if full?
+    @bikes.push(bike)
+  end
+  # def bike
+  #   @bike
+  # end
+  private
+    def full?
+      @bikes.length >= @capacity
+    end
+
+    def empty?
+      @bikes.empty?
+    end
 end
 
 # docking_station = DockingStation.new
-# docking_station.release_bike
+# b = docking_station.release_bike
+# puts b.working?
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,14 +2,62 @@ require "docking_station"
 require "bike"
 
 describe DockingStation do
- # it 'releases bike' do
- #   expect((DockingStation.new).release_bike).to eq("Bike released!")
+
+ # it 'responds to method release_bike' do
+ #   expect(subject).to respond_to(:release_bike)
  # end
+
  it { is_expected.to respond_to :release_bike }
 
- it 'release the working bike' do
-   # expect(subject.release_bike).to eq(Bike.new)
-   bike = subject.release_bike
-   expect(bike).to be_working
+ it { is_expected.to respond_to(:dock).with(1).argument }
+
+ it { is_expected.to respond_to(:bikes) }
+ it { is_expected.to respond_to(:capacity) }
+
+ it "sets default capacity as 20" do
+   docking_station = DockingStation.new
+   expect(docking_station.capacity).to eq(20)
+ end
+
+ it "sets new capacity when given argument" do
+   docking_station = DockingStation.new(30)
+   expect(docking_station.capacity).to eq(30)
+ end
+
+ describe '#release_bike' do
+   it 'release the working bike' do
+     # expect(subject.release_bike).to eq(Bike.new)
+     bike = Bike.new
+     # bike = subject.release_bike
+     expect(bike).to be_working
+   end
+
+   it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+   end
+
+   it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+   end
+ end
+
+ describe '#dock' do
+   it 'docks bike' do
+     bike = Bike.new
+     subject.dock(bike)
+     expect(subject.bikes).to eq subject.bikes # bikes is method from attr_reader
+   end
+
+   it 'raises error when dock station is full' do
+     # bike = Bike.new
+     # subject.dock(bike)
+     # bike2 = Bike.new
+
+     subject.capacity.times {subject.dock(Bike.new)}
+
+     expect { subject.dock(Bike.new)}.to raise_error "Dock station is full"
+   end
  end
 end
